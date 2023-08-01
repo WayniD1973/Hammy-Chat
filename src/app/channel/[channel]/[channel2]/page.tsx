e1076650b7b557df3cae802835767570365f266d
WayniD1973 <77841893+WayniD1973@users.noreply.github.com>
2023-08-01 18:21:36 +0000

    Potentially fixed some things

--- src/app/channel/[channel]/[channel2]/page.tsx
+++ src/app/channel/[channel]/[channel2]/page.tsx
@@ -1,4 +1,5 @@
-export default function page({params}: { params: { channel: number, channel2: number } }) {
+//app/channel/[channel]/[channel2]page.tsx
+export default function page({params}: { params: { channel: string, channel2: string } }) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       page2
--- src/app/channel/[channel]/page.tsx
+++ src/app/channel/[channel]/page.tsx
@@ -1,4 +1,5 @@
-export default function Page({params}: { params: { channel: number } }) {
+//app/channel/[channel]/page.tsx
+export default function Page({params}: { params: { channel: string } }) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       page1
